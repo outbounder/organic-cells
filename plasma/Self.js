10ea68a852a2bf2d3dc9d2a041f940d83bcfdb7d
Boris Filipov <obiwonn@gmail.com>
2013-01-15 01:52:05 +0200

    added upgrade and restartSiblings support

--- plasma/Self.js
+++ plasma/Self.js
@@ -1,6 +1,9 @@
 var Organel = require("organic").Organel;
 var _ = require("underscore");
 var path = require('path');
+var shelljs = require("shelljs");
+var fs = require('fs');
+var async = require('async');
 
 module.exports = Organel.extend(function Self(plasma, config){
   Organel.call(this, plasma, config);
@@ -10,7 +13,13 @@ module.exports = Organel.extend(function Self(plasma, config){
   this.config = config;
   var self = this;
 
+  process.on("SIGUSR1", function(){
+    console.log("recieved upgrade signal");
+    self.upgrade({});
+  });
+
   process.on("SIGUSR2", function(){
+    console.log("recieved restart signal");
     self.restart({});
   });
 
@@ -18,27 +27,39 @@ module.exports = Organel.extend(function Self(plasma, config){
     this[c.action](c, sender, callback);
   });
 
-  if(config.siblings) {
+  if(config.siblings)
+    self.startSiblings(config, this, function(c){
+      self.emit({type: "Self", pid: process.pid, siblings: c.startedSiblings});
+    });
+
+}, {
+  startSiblings: function(c, sender, callback){
+    var self = this;
     this.emit({
       type: "Tissue",
       action: "list",
-      target: config.tissue
+      target: c.tissue
     }, function(list){
-      var siblingsNames = _.pluck(config.siblings,"name");
+      var siblingsNames = _.pluck(c.siblings,"name");
       var startedNames = _.pluck(list.data, "name");
       var notStartedSiblings = _.difference(siblingsNames, startedNames);
-      notStartedSiblings.forEach(function(siblingName){
+      var startedSiblings = [];
+      async.forEach(notStartedSiblings, function(siblingName, next){
         var target = path.join(path.dirname(process.argv[1]), siblingName);
         self.emit({
           type: "Tissue",
           action: "start",
           target: target
+        }, function(started){
+          if(started instanceof Error) return next(started);
+          startedSiblings.push(started.data);
+          next();
         });
+      }, function(err){
+        if(callback) callback(err || {startedSiblings: startedSiblings});
       });
     });
-  }
-
-}, {
+  },
   restart: function(c, sender, callback) {
     this.emit({
       type: "Tissue",
@@ -47,6 +68,21 @@ module.exports = Organel.extend(function Self(plasma, config){
     }, function(start){
       if(callback) callback(start);
       process.exit();
+    });
+  },
+  upgrade: function(c, sender, callback){
+    var self = this;
+    fs.exists(".git", function(exists){
+      if(exists) {
+        shelljs.exec("git pull",function(code, output){
+          console.log(output);
+          if(code == 0)
+            shelljs.exec("npm install", function(code, output){
+              console.log(output);
+              self.restart(c, sender, callback);
+            });
+        });
+      }
     });
   }
 })
